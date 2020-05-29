27102d14c54e8751ea1a786136cdb3badac9d8af
jimzers <adamleesaintfrancis@gmail.com>
2020-05-29 06:05:22 -0700

    Updates

---
+++ precache-manifest.2cb27a62a3352489da06b74a5dcd0c08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b34e9c486843e6e89b98b4d7d0913fa",
+    "url": "/jimzers-blog/index.html"
+  },
+  {
+    "revision": "7bd7561ef58f63d31d58",
+    "url": "/jimzers-blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8563ae7b50a4c108f277",
+    "url": "/jimzers-blog/static/js/2.334b384e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jimzers-blog/static/js/2.334b384e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd7561ef58f63d31d58",
+    "url": "/jimzers-blog/static/js/main.8a600f9f.chunk.js"
+  },
+  {
+    "revision": "19e0dc49b5a70d250ecb",
+    "url": "/jimzers-blog/static/js/runtime-main.8b1a4b67.js"
+  }
+]);
